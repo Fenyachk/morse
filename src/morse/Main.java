cea7edec61358819e5e2c70b558106fbb9229ff5
Fenya <lena28d@mail.ru>
2024-06-30 13:43:25 +0300

    Morse code translator
     v 1.0 Use .bat file to run the program

---
+++ src/morse/Main.java
@@ -0,0 +1,14 @@
+package morse;
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Введите текст для перевода:");
+        String text = scanner.nextLine();
+        String morseCode = MorseTranslate.getMorseCode(text);
+        System.out.println("Текст '" + text + "' в коде Морзе: " + morseCode);
+    }
+}
