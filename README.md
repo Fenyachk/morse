a023751a3510e2e81875180270b8955d1157312b
Fenya <lena28d@mail.ru>
2024-06-30 13:43:25 +0300

    Morse code translator
     v 1.0 Release. Translation into morse code from English

---
+++ README.md
@@ -0,0 +1,3 @@
+# morse
+
+Translation into morse code from English. Use .bat file to run the program.
---
+++ src/morse/MorseTranslate.java
@@ -0,0 +1,45 @@
+package morse;
+
+import java.util.*;
+
+public class MorseTranslate {
+    private static final Map<Character, String> morseMap = new HashMap<>();
+    private ArrayList<String> morseCode;
+
+    static {
+        morseMap.put('a', ".-");
+        morseMap.put('b', "-...");
+        morseMap.put('c', "-.-.");
+        morseMap.put('d', "-..");
+        morseMap.put('e', ".");
+        morseMap.put('f', "..-.");
+        morseMap.put('g', "--.");
+        morseMap.put('h', "....");
+        morseMap.put('i', "..");
+        morseMap.put('j', ".---");
+        morseMap.put('k', "-.-");
+        morseMap.put('l', ".-..");
+        morseMap.put('m', "--");
+        morseMap.put('n', "-.");
+        morseMap.put('o', "---");
+        morseMap.put('p', ".--.");
+        morseMap.put('q', "--.-");
+        morseMap.put('r', ".-.");
+        morseMap.put('s', "...");
+        morseMap.put('t', "-");
+        morseMap.put('u', "..-");
+        morseMap.put('v', "...-");
+        morseMap.put('w', ".--");
+        morseMap.put('x', "-..-");
+        morseMap.put('y', "-.--");
+        morseMap.put('z', "--..");
+    }
+    public static String getMorseCode(String text) {
+        StringBuilder morseCode = new StringBuilder();
+        text = text.toLowerCase();
+        for (char c : text.toCharArray()) {
+            morseCode.append( morseMap.getOrDefault(c, ""));
+        }
+        return morseCode.toString();
+    }
+}
